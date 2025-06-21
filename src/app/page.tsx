43f1631732d1848185ec29b44c079502a7791ca9
aditya-23140 <ironaditya12345@gmail.com>
2025-06-21 17:18:00 +0530

    Implement vapi ai for conversation and dynamically fetch data to show on home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,40 +2,36 @@ import React from "react";
 import CompanionCard from "@/components/CompanionCard";
 import CompanionsList from "@/components/CompanionsList";
 import CTA from "@/components/CTA";
-import { recentSessions } from "@/constants";
+import {
+  getAllCompanions,
+  getRecentSessions,
+} from "@/lib/actions/companions.action";
+import { getSubjectColor } from "@/lib/utils";
 
-const Page = () => {
+const Page = async () => {
+  const companions = await getAllCompanions({ limit: 3 });
+  const recentSessions = await getRecentSessions(10);
+
   return (
     <main>
       <h1>Popular Companions</h1>
       <section className="home-section">
-        <CompanionCard
-          id="123"
-          name="Neura the Brainy Explorer"
-          topic="Topic: Derivatives & Integrals"
-          subject="science"
-          duration={45}
-          color="#E5D0FF"
-        />
-        <CompanionCard
-          id="456"
-          name="Countsy the Number Wizard"
-          topic="Topic: Derivatives & Integrals"
-          subject="maths"
-          duration={30}
-          color="#FFDA6E"
-        />
-        <CompanionCard
-          id="789"
-          name="Verba the Vocabulary Builder"
-          topic="Topic: English Literature "
-          subject="language"
-          duration={30}
-          color="#BDE7FF"
-        />
+        {companions.map((companion) => {
+          return (
+            <CompanionCard
+              key={companion.id}
+              {...companion}
+              color={getSubjectColor(companion.subject)}
+            />
+          );
+        })}
       </section>
       <section className="home-section2">
-        <CompanionsList title="Recently completed sessions" companions={recentSessions} classNames="w-2/3 max-lg:w-full" />
+        <CompanionsList
+          title="Recently completed sessions"
+          companions={recentSessions}
+          classNames="w-2/3 max-lg:w-full"
+        />
         <CTA />
       </section>
     </main>
